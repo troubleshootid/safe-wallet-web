055aab19ff2b61c57013a68ae3612444165e393c
troubleshootid <kumarpyrrhos@gmail.com>
2025-11-14 07:46:05 +0900

    Allow Cloudflare Insights in CSP

--- apps/web/src/config/securityHeaders.ts
+++ apps/web/src/config/securityHeaders.ts
@@ -15,7 +15,7 @@ const isCypress = Boolean(typeof window !== 'undefined' && window.Cypress)
 export const ContentSecurityPolicy = `
  default-src 'self';
  connect-src 'self' *;
- script-src 'self' 'unsafe-inline' https://*.getbeamer.com https://www.googletagmanager.com https://*.ingest.sentry.io https://sentry.io ${
+ script-src 'self' 'unsafe-inline' https://*.getbeamer.com https://www.googletagmanager.com https://*.ingest.sentry.io https://sentry.io https://static.cloudflareinsights.com ${
    !IS_PRODUCTION || isCypress
      ? "'unsafe-eval'" // Dev server and cypress need unsafe-eval
      : "'wasm-unsafe-eval'"
